fa66ab04ac3709919757224e15a505cc35e68736
wzb <wangzb@zhinanmao.com>
2018-11-05 16:29:22 +0800

    第一次提交

---
+++ TestLibrary/app/src/main/java/com/example/wzb/testlibrary/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.wzb.testlibrary
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        Log.i("out", "这里啥也没有，就是拿来测试的")
+    }
+
+    fun test(){
+
+    }
+}
